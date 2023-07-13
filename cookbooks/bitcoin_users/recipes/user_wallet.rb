9528021871fb25617ac9e8a28db2a66a158dde9e
Kelvin Lu <kelvinlu4@gmail.com>
2023-07-13 00:50:26 -0700

    Adjust home directory for Sparrow recipe

--- cookbooks/bitcoin_users/recipes/user_wallet.rb
+++ cookbooks/bitcoin_users/recipes/user_wallet.rb
@@ -3,27 +3,23 @@
 # Recipe:: guest_user_wallet
 #
 
-root_location = node['bitcoin_users'].fetch('root_location', '/home')
-
 [
   node['bitcoin_users'].fetch('personal_user'),
   node['bitcoin_users'].fetch('guest_user')
 ].each do |params|
   username = params.fetch('name')
 
-  home_dir = File.join(root_location, username, 'home/', username)
-
-  directory File.join(home_dir, '.sparrow') do
+  directory File.join(Dir.home(username), '.sparrow') do
     user lazy { Etc.getpwnam(username).uid }
     group lazy { Etc.getpwnam(username).gid }
     mode '0750'
   end
 
-  link File.join(home_dir, '.sparrow', 'wallets') do
-    to File.join(home_dir, 'workspace')
+  link File.join(Dir.home(username), '.sparrow', 'wallets') do
+    to File.join(Dir.home(username), 'workspace')
   end
 
-  cookbook_file File.join(home_dir, '.sparrow', 'config') do
+  cookbook_file File.join(Dir.home(username), '.sparrow', 'config') do
     source 'sparrow-config'
 
     user lazy { Etc.getpwnam(username).uid }
