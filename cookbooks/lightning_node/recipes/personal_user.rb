42c111299537a40b9342e33c1623d0c8542f4b7e
Kelvin Lu <kelvinlu4@gmail.com>
2023-07-16 00:24:19 -0700

    Add basic lnd-scb.service systemd calls to personal user sudoers

--- cookbooks/lightning_node/recipes/personal_user.rb
+++ cookbooks/lightning_node/recipes/personal_user.rb
@@ -55,6 +55,14 @@ sudo '56-lnd-tor-personal-user' do
   ]
 end
 
+sudo '57-lnd-scb-personal-user' do
+  user username
+
+  commands [
+    '/usr/bin/systemctl start lnd-scb.service',
+    '/usr/bin/systemctl stop lnd-scb.service',
+  ]
+end
 
 file File.join(Dir.home(username), 'run-lnd') do
   group lazy { Etc.getpwnam(username).gid }
@@ -299,5 +307,35 @@ file File.join(Dir.home(username), 'view-lnd-rest-onion-hostname') do
   content <<~BASH
     #!/bin/bash
     sudo cat /var/lib/tor/hidden_service_lnd_rest/hostname
+  BASH
+end
+
+file File.join(Dir.home(username), 'start-lnd-scb') do
+  group lazy { Etc.getpwnam(username).gid }
+  mode '0750'
+
+  content <<~BASH
+    #!/bin/bash
+    sudo systemctl start lnd-scb.service
+  BASH
+end
+
+file File.join(Dir.home(username), 'stop-lnd-scb') do
+  group lazy { Etc.getpwnam(username).gid }
+  mode '0750'
+
+  content <<~BASH
+    #!/bin/bash
+    sudo systemctl stop lnd-scb.service
+  BASH
+end
+
+file File.join(Dir.home(username), 'status-lnd-scb') do
+  group lazy { Etc.getpwnam(username).gid }
+  mode '0750'
+
+  content <<~BASH
+    #!/bin/bash
+    systemctl status lnd-scb.service
   BASH
 end
