01eb0aefa3b59ff1a8631e97ca005ba5cabafcbc
Kelvin Lu <kelvinlu4@gmail.com>
2023-07-19 20:45:35 -0700

    Disable default zswap, install zram module

--- cookbooks/rpi4_server/recipes/cmdline.rb
+++ cookbooks/rpi4_server/recipes/cmdline.rb
@@ -6,12 +6,14 @@
 CMDLINE_TXT         = '/boot/firmware/cmdline.txt'
 
 SCREENSAVER_NOBLANK = 'consoleblank=0'
+ZSWAP_DISABLE       = 'zswap.enabled=0'
 
 ruby_block "Set Linux cmdline (#{CMDLINE_TXT})" do
   block do
     parameters = File.read(CMDLINE_TXT).split
 
     parameters.append(SCREENSAVER_NOBLANK) unless parameters.include?(SCREENSAVER_NOBLANK)
+    parameters.append(ZSWAP_DISABLE) unless parameters.include?(ZSWAP_DISABLE)
 
     File.write(CMDLINE_TXT, parameters.join(' '))
   end
--- cookbooks/rpi4_server/recipes/zram.rb
+++ cookbooks/rpi4_server/recipes/zram.rb
@@ -7,6 +7,10 @@ swap_params = node['rpi4_server']&.[]('zram_swap')
 devices = swap_params['devices']
 size_mb = swap_params['uncompressed_limit_mb']
 
+apt_package 'linux-modules-extra-raspi' do
+  action :install
+end
+
 apt_package 'zram-tools' do
   action :install
 end
@@ -15,13 +19,9 @@ ruby_block 'configure /etc/default/zramswap' do
   block do
     file = Chef::Util::FileEdit.new('/etc/default/zramswap')
 
-    config_line = "CORES=#{devices}"
-    file.search_file_replace_line(/^CORES=\d+$/, config_line)
-    file.insert_line_if_no_match(/^CORES=\d+$/, config_line)
-
-    config_line = "ALLOCATION=#{size_mb}"
-    file.search_file_replace_line(/^ALLOCATION=\d+$/, config_line)
-    file.insert_line_if_no_match(/^ALLOCATION=\d+$/, config_line)
+    config_line = "SIZE=#{size_mb}"
+    file.search_file_replace_line(/^SIZE=\d+$/, config_line)
+    file.insert_line_if_no_match(/^SIZE=\d+$/, config_line)
 
     file.write_file
   end
